f8b5e514c5b4d47dbd9606bc20795d1e0abe1108
maoyifei <maoyifei@magiccompass.cn>
2018-04-05 18:08:41 +0800

    优化断言逻辑，当在提交操作时出现错误时，截图;直接返回上一页面，不影响其他通道测试

--- XSTZ_Test_framework/src/test/page/xstz_payway_select.py
+++ XSTZ_Test_framework/src/test/page/xstz_payway_select.py
@@ -3,6 +3,10 @@
 '''
 - payment way page class
 - 继承XSTZLoginPage登录页面封装
+
+update:
+    5/04/2018 优化断言逻辑，当在提交操作时出现错误时，截图，
+                                直接返回上一页面，不影响其他通道测试
 '''
 import time
 from selenium.webdriver.common.by import By
@@ -21,7 +25,7 @@ class ChannelSelect(InjectionPage):
     loc_bank_list = (By.CLASS_NAME, 'w-select-ul')               # 定位可选银行区域
     loc_bank_detail = (By.TAG_NAME, 'li')                        # 银行明细
 
-    def select_payway(self, cn=0):
+    def select_payway(self, cn):
         '''pay way name method'''
         try:
             self.payways_elements[cn].click()                       # 选择支付通道一
@@ -30,19 +34,30 @@ class ChannelSelect(InjectionPage):
         except Exception:
             logger.error("Payway does not exist")
 
-    def sumbit(self, money):
+    def input_money(self, money):
         '''提交操作'''
         try:
             self.clear(self.loc_input_money)
             self.type(money, self.loc_input_money)
+
+            logger.info("input inject money:{}...".format(money))
+        except Exception:
+            logger.error("提交跳转出错")
+
+    @property
+    def submit(self):
+        '''
+        return:
+            success: True
+            fail: False, back last page
+        '''
+        try:
             self.click(self.loc_next_step)
             self.click(self.loc_submit_button)
-            # self.switch_to_altet                                #
-            # 接受弹窗，此处为提交时，浏览器安全提示
-            time.sleep(2)
-            logger.info("jumping to payment page 。。。")
-        except Exception as e:
-            logger.error("Error .Reason:{}".format(e))
+            self.switch_to_altet               # 接受弹窗，此处为提交时，浏览器安全提示
+            time.sleep(1)
+        except Exception:
+            raise Exception("Submit fail")
 
     @property
     def exchange_bank_page(self):
@@ -62,17 +77,21 @@ class ChannelSelect(InjectionPage):
     def close_bank_page(self, bank_handle):
         '''close bank page,return to payment page'''
         try:
-            self.close                              # 关闭第三方支付页面
-            self.exchange_to_window(bank_handle)    # 返回支付页面
+            self.close                                         # 关闭第三方支付页面
+            self.exchange_to_window(bank_handle)               # 返回支付页面
             logger.info("return to payment page!")
         except Exception as e:
+            pass
             logger.error("Error ！Reason:{}".format(e))
 
     @property
     def return_payment_page(self):  # 点击确认，已完成付款！返回支付页面
-        self.click(self.loc_ensure_pay)
+        try:
+            self.click(self.loc_ensure_pay)
+        except Exception:
+            pass
 
-    def select_bank(self, bn):
+    def select_bank(self, bn):  # 当支付通道存在银行下拉菜单选项时
         self.click(self.loc_bank_inbox)
         banks = self.find_element(
             self.loc_bank_list).find_elements(self.loc_bank_detail)  # 二次定位
@@ -80,30 +99,46 @@ class ChannelSelect(InjectionPage):
         return banks[bn].text
 
     def is_deposit_success(self, locator, element):
-
-        bank_handle = self.exchange_bank_page
+        '''
+        Return:
+            Payload , is the source data for send Email,Dingding Robot, 
+            insert Influxdb
+        '''
         try:
-            self.find_element((locator, element))
-            IsSuccess = "success"
-            ReturnCode = 0
-            result = True
-            error_info = None
-            print("Test Success!\n ")
-            logger.info("第三方支付跳转成功")
+            self.submit                                # 判断是否可以正常进行提交
         except Exception:
-            error_info = self.save_screen_shot()  # 错误时保存截图:图片名称
+            error_info = self.save_screen_shot()       # 错误时保存截图:图片名称
             ReturnCode = 1
             IsSuccess = "Fail"
             result = False
-            print("Test fail!")
-            logger.error("支付页面崩溃或超时!")
+            self.back()                                # 返回上一页面
+        else:                                          # 如果可以正常提交，进入第三支付页面，异常判断
+            try:
+                bank_handle = self.exchange_bank_page  # 跳转第三方网银支付页面操作
+                self.find_element((locator, element))
+                IsSuccess = "success"
+                ReturnCode = 0
+                result = True
+                error_info = None
+                print("Test Success!\n ")
+                logger.info("第三方支付跳转成功")
+            except Exception:
+                error_info = self.save_screen_shot()  # 错误时保存截图:图片名称
+                ReturnCode = 1
+                IsSuccess = "Fail"
+                result = False
+                print("Test fail!")
+                logger.error("支付页面崩溃或超时!")
+            finally:
+                self.close_bank_page(bank_handle)
+                self.return_payment_page
+
         finally:
-            self.close_bank_page(bank_handle)
-            self.return_payment_page
             payload = {"result": result,
                        "data": {"ReturnCode": ReturnCode,
                                 "IsSuccess": IsSuccess,
                                 },
                        "Exception": error_info
                        }
+
             return payload
