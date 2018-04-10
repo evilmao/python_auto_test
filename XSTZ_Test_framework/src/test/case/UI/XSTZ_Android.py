5d8e60b1f8ac8fba8d01e37d411a17a1258574cf
maoyifei <maoyifei@magiccompass.cn>
2018-04-10 18:09:43 +0800

    优化代码

--- XSTZ_Test_framework/src/test/case/UI/XSTZ_Android.py
+++ XSTZ_Test_framework/src/test/case/UI/XSTZ_Android.py
@@ -8,26 +8,16 @@ import os
 import time
 import unittest
 from appium import webdriver
-from src.test.common.subfunc import get_func_name
 from src.test.page.app_login_page import LoginPage
-from src.utils.HTMLTestRunner_Echarts import HTMLTestRunner
-from src.utils.config import Config, REPORT_PATH
+from src.utils.config import Config
+from src.utils.support import get_func_name
 
 
-REPORT_URL = Config().get("report_url")
-now = time.strftime("%Y%m%d%H%M", time.localtime())
-report_name = "report_%s.html" % (now)
-report_path = os.path.join(REPORT_PATH, report_name)
-report_url = REPORT_URL % report_name  # 重新定义report_url
-
-Test_date = time.strftime('%Y-%m-%d %H:%M:%S')
-data = {"suite": {},
-        "Test_date": Test_date,
-        "report_url": report_url
-        }
+Android_data = {"suite": {}}
 
 
 class XSTZ_Android(unittest.TestCase):
+    '''Android端测试'''
     USERNAME = Config().get("USERNAME")
     PWD = Config().get("PWD")
     desired_caps = Config().get("desired_caps")
@@ -37,9 +27,9 @@ class XSTZ_Android(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print("starting test on Android...")
+        print("Android starting test...")
 
-    def test_Android_login(self):
+    def test_Android_1_login(self):
         '''Android 登录测试'''
         test_action = get_func_name()
         Login = LoginPage(self.driver)
@@ -48,7 +38,7 @@ class XSTZ_Android(unittest.TestCase):
         result = Login.is_login_success
         data1 = {}
         data1[test_action] = result["data"]
-        data["suite"].update(data1)
+        Android_data["suite"].update(data1)
         self.assertTrue(result["result"], msg=result["Exception"])
 
     @classmethod
@@ -57,13 +47,30 @@ class XSTZ_Android(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    from src.utils.HTMLTestRunner_Echarts import HTMLTestRunner
+    from src.utils.config import REPORT_PATH
+    from src.utils.DingDingReport import ding_report
+    from src.utils.Insert_InfluxDB import insert_influxdb
+
+    REPORT_URL = Config().get("report_url")
+    now = time.strftime("%Y%m%d%H%M", time.localtime())
+    report_name = "report_%s.html" % (now)
+    report_path = os.path.join(REPORT_PATH, report_name)
+    report_url = REPORT_URL % report_name  # 重新定义report_url
+    Test_date = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    Android_data["report_url"] = report_url
+    Android_data["Test_date"] = Test_date
+
     title = "鑫圣投资"
-    description = 'Andriud端测试'
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(XSTZ_Android, "test_login"))
+    description = 'Android端测试'
+    test_suite = unittest.TestSuite(unittest.makeSuite(XSTZ_Android))
 
     with open(report_path, 'wb') as f:
         runner = HTMLTestRunner(
             f, verbosity=2, title=title, description=description)
         runner.run(test_suite)
-    print(data)
+
+    insert_influxdb(Android_data)                                # 插入数据库
+    ding_report(**Android_data)                                  # 钉钉机器人自动播报
+    print(Android_data)
